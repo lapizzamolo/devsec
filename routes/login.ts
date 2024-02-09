4cba33beb3e777035df2a60ba4052c64cab572c1
lapizzamolo <lerouxmarin7@gmail.com>
2024-02-09 09:15:15 +0100

    Fixed injection sql

--- routes/login.ts
+++ routes/login.ts
@@ -33,7 +33,15 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    const email = req.body.email
+    const password = req.body.password
+    models.sequelize.query('SELECT * FROM Users WHERE email = ? AND password = ? AND deletedAt IS NULL',
+      {
+        model: UserModel,
+        plain: true,
+        replacements: [email || '',
+          security.hash(password || '')]
+      })
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
